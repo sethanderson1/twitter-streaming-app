3cd11a8aed1497d1a10dc620da22bedef7896b34
Seth Anderson <sethdanderson17@gmail.com>
2022-05-28 19:13:29 -0700

    Took server.listen out of mongoose.connect

--- components/CardList.js
+++ components/CardList.js
@@ -3,28 +3,44 @@ import socketIOClient from "socket.io-client";
 import { Card } from "./Card";
 import styled from "styled-components";
 import { WidgetCard } from "./WidgetCard";
+import axios from "axios";
 
 const ENDPOINT = "http://localhost:8000";
 
-const TweetStream = styled.div`
-  font-family: "Roboto", sans-serif;
-`;
-
 export const CardList = () => {
   const [tweets, setTweets] = useState([]);
+  const [socketOn, setSocketOn] = useState(false);
+  console.log("socketOn", socketOn);
   console.log("tweets", tweets);
 
   useEffect(() => {
     const socket = socketIOClient(ENDPOINT);
     socket.on("tweet", (data) => {
-      console.log("tweets in useEffect", tweets);
-      console.log("data", data);
       setTweets((prevTweets) => [...prevTweets, data]);
     });
   }, []);
 
+  const handleSocketClick = async () => {
+    // turn socket off on server
+    console.log("clicked socket on or off")
+    const url = `http://localhost:8000/socketOn`;
+    try {
+      const res = await axios.post(url, {
+        socketOn: !socketOn,
+      });
+      console.log("res", res);
+
+      setSocketOn((prev) => !prev);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
   return (
-    <TweetStream>
+    <div>
+      <div>
+        <button onClick={handleSocketClick}>{socketOn ? "On" : "Off"}</button>
+      </div>
       {tweets
         .map((tweet, i) => {
           console.log("tweet", tweet);
@@ -34,6 +50,6 @@ export const CardList = () => {
           );
         })
         .reverse()}
-    </TweetStream>
+    </div>
   );
 };
